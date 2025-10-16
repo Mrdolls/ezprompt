21ba37b0f1ca7694060d7da2cd32023278b3a4a9
Riyane El Qoqui <riyane.dev@gmail.com>
2025-10-16 17:04:18 +0200

    feat(utils): enhance `ft_memcpy` for memory overlap handling and safety

--- src/utils/ft_memcpy.c
+++ src/utils/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mgingast <mgingast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 21:25:31 by mgingast          #+#    #+#             */
-/*   Updated: 2025/10/16 12:56:15 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/10/16 16:49:17 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,28 @@ void	*ft_memcpy(void *dest, const void *src, const size_t n)
 	unsigned char		*d;
 	size_t				i;
 
+	if (n == 0)
+		return (dest);
 	if (!dest || !src)
-		return (NULL);
+		return (dest);
 	s = (const unsigned char *)src;
 	d = (unsigned char *)dest;
-	i = 0;
-	while (i < n)
+	if (d == s)
+		return (dest);
+	if (d > s && d < s + n)
 	{
-		d[i] = (unsigned char)s[i];
-		i++;
+		i = n;
+		while (i-- > 0)
+			d[i] = s[i];
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
 	}
 	return (dest);
 }
